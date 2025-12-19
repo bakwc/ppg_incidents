07f04f5e804500f1437af1feddeb3df850a142ca
Filipp Ozinov <filipp@bakanov.su>
2025-12-20 02:48:21 +0400

    Production options

--- ppg_incidents/settings.py
+++ ppg_incidents/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5))0ez+u^@hf2()mmejki3e^h)d5d9)3ommw(fes+!#gun!zq^"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-5))0ez+u^@hf2()mmejki3e^h)d5d9)3ommw(fes+!#gun!zq^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['ppg-incidents.org', 'www.ppg-incidents.org', 'localhost', '127.0.0.1']
 
